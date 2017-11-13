4ed07b57f135c930fcecb5a4bd357315f485034d
arshadsaidoo <a_saidoo@encs.concordia.ca>
2017-11-13 15:52:55 -0500

    Update urls.py

--- django_youtube/urls.py
+++ django_youtube/urls.py
@@ -1,7 +1,7 @@
 try:
     from django.conf.urls import patterns, url
 except ImportError:
-    from django.conf.urls.defaults import patterns, url
+    rom django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('django_youtube.views',
     # list of the videos
